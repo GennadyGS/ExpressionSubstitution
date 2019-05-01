4696e2318bac991579f5dccbedb3d616c0e38764
Gennadii Saltyshchak <GennadyGS@gmail.com>
2019-05-01 13:04:26 +0300

    Add FieldReplacer

---
+++ src/ReplaceExpression.Console/FieldReplacerVisitor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ReplaceExpression.Console
+{
+    internal class FieldReplacerVisitor : ExpressionVisitor
+    {
+        private readonly IReadOnlyDictionary<string, LambdaExpression> _fieldExpressionMap;
+        private static readonly MethodInfo GetItemMethodInfo = typeof(IDictionary<string, object>).GetMethod("get_Item");
+
+        public FieldReplacerVisitor(IReadOnlyDictionary<string, LambdaExpression> fieldExpressionMap) =>
+            _fieldExpressionMap = fieldExpressionMap;
+
+        protected override Expression VisitMethodCall(MethodCallExpression methodCall) =>
+            methodCall.Method == GetItemMethodInfo
+            && methodCall.Object.NodeType == ExpressionType.Parameter
+            && methodCall.Arguments.Single() is ConstantExpression constant
+            && _fieldExpressionMap.TryGetValue((string)constant.Value, out var fieldExpression)
+                ? fieldExpression.Body
+                : base.VisitMethodCall(methodCall);
+    }
+}
--- src/ReplaceExpression.Console/Program.cs
+++ src/ReplaceExpression.Console/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace ReplaceExpression.Console
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace ReplaceExpression.Console
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            Expression<Func<IDictionary<string, object>, object>> expr = record => (int)record["a"] + 5;
+            var fieldExpressionMap = new Dictionary<string, LambdaExpression>
+            {
+                ["a"] = (Expression<Func<IDictionary<string, object>, object>>)(record => (int)record["b"] + (int)record["c"]),
+            };
+            var modifiedExpr = new FieldReplacerVisitor(fieldExpressionMap).Visit(expr);
+            System.Console.WriteLine(expr.ToString());
+            System.Console.WriteLine(modifiedExpr.ToString());
         }
     }
 }
